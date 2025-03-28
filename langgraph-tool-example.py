b1c509e0bd95f3bba82a38036e33a472eeade652
adonem <@>
2025-03-27 21:31:08 -0700

    Langgraph tool example.

--- langgraph-tool-example.py
+++ langgraph-tool-example.py
@@ -1,64 +1,148 @@
-# example from: https://api.python.langchain.com/en/latest/agents/langchain.agents.react.agent.create_react_agent.html
-# with tools from https://python.langchain.com/docs/how_to/output_parser_string/
+#!/usr/bin/env python3
+# In[ ]:
+# Example from: https://langchain-ai.github.io/langgraph/how-tos/tool-calling/#setup
 # modified to use ChatLlamaCpp
-# https://python.langchain.com/v0.2/api_reference/community/chat_models/langchain_community.chat_models.llamacpp.ChatLlamaCpp.html
 
-from langchain_community.chat_models import ChatLlamaCpp
-from langchain.agents import AgentExecutor, create_react_agent
-from langchain_core.tools import tool
-from langchain.callbacks.manager import CallbackManager
 
-from langchain_core.prompts import PromptTemplate
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+# In[ ]:
+get_ipython().system('pip install langchain langchain-community langchain-core langgraph')
 
+
+# In[ ]:
+get_ipython().system('pip install huggingface-hub')
+
+
+# In[ ]:
 from huggingface_hub import hf_hub_download
 
 #hf_gguf_model = ["bartowski/Llama-3.2-1B-Instruct-GGUF", "Llama-3.2-1B-Instruct-Q4_K_M.gguf" ]
 #hf_gguf_model = ["bartowski/Llama-3.2-3B-Instruct-GGUF", "Llama-3.2-3B-Instruct-Q4_K_M.gguf"]
-hf_gguf_model = ["bartowski/Meta-Llama-3.1-8B-Instruct-GGUF", "Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf"]
+hf_gguf_model = ["Qwen/Qwen2.5-3B-Instruct-GGUF", "qwen2.5-3b-instruct-q4_k_m.gguf"]
 model_path = hf_hub_download(*hf_gguf_model)
+
+
+# In[ ]:
+get_ipython().system('pip install llama-cpp-python')
+
+
+# In[ ]:
+from langchain_community.chat_models import ChatLlamaCpp
 
 model = ChatLlamaCpp(
     model_path=model_path,
-    callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
     verbose=False,
-) 
+    stop=["<|end_of_text|>", "<|eot_id|>"],
+)
+
+
+# In[ ]:
+from langchain_core.tools import tool
+from langgraph.prebuilt import ToolNode
+from datetime import datetime
 
 @tool
 def get_weather(location: str) -> str:
-    """Get the weather from a location."""
+    """Get the current weather for a location."""
+    if location.lower() in ["sf", "san francisco"]:
+        return "60 degrees and foggy."
+    else:
+        return "80 degrees and sunny."
 
-    return "Sunny."
+@tool
+def get_current_time() -> str:
+    """This tool returns the current time."""
+    
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return current_time
 
-tools = [get_weather]
+tools = [get_weather, get_current_time]
 
-template = '''Answer the following questions as best you can. You have access to the following tools:
+model_with_tools = model.bind_tools(tools)
+tool_node = ToolNode(tools)
 
-{tools}
 
-Use the following format:
+# In[ ]:
+import re
+import uuid
+import json
+from langgraph.graph import MessagesState
 
-Question: the input question you must answer
-Thought: you should always think about what to do
-Action: the action to take, should be one of [{tool_names}]
-Action Input: the input to the action
-Observation: the result of the action
-... (this Thought/Action/Action Input/Observation can repeat N times)
-Thought: I now know the final answer
-Final Answer: the final answer to the original input question
+def json_strip(s):
+    count = 0
+    start = None
+    end = None
 
-Begin!
+    for i, c in enumerate(s):
+        if c == '{': count += 1
+        else:
+            if start is None and count:
+                count = 1
+                start = i - 1
+            if c == '}': count -= 1
+        if count == 0:
+            end = i + 1
 
-Question: {input}
-Thought:{agent_scratchpad}'''
+    if end and start is not None: s = s[start:end]
+    return s
 
-prompt = PromptTemplate.from_template(template)
-
-agent = create_react_agent(model, tools, prompt)
-agent_executor = AgentExecutor(agent=agent, tools=tools)
-
-agent_executor.invoke(
-    {
-        "input": "What's the weather in San Francisco, CA?"
+def json_tool_call(s):
+    s = json_strip(s.replace('\\"', '"').replace("'", '"').replace('}"', '}').replace('"{', '{'))
+    tool_call_data = json.loads(s)
+    tool_params = tool_call_data.get("arguments", tool_call_data.get("parameters", {}).get("properties", tool_call_data.get("parameters", {})))
+    return {
+        "name": tool_call_data["name"],
+        "args": tool_params,
+        "id": str(uuid.uuid4()),
+        "type": "tool_call"
     }
-)
+
+def fix_tool_calls(state: MessagesState):
+    if ( state.tool_calls is not None and 0 != len(state.tool_calls)) or state.content is None: return state
+
+    content = state.content.strip()
+    m = re.findall(r'<tool_call>\s*(\{.*\})\s*</tool_call>', content, re.DOTALL)
+    if m is None or 0 == len(m):
+        m = re.findall(r'<tool_call>\s*(\{.*\})', content, re.DOTALL)
+    try:
+        if m is not None and len(m) > 0:
+            state.tool_calls = [json_tool_call(s) for s in m]
+            state.content = ""
+        elif content[0] == '{':
+            state.tool_calls = [ json_tool_call(content) ]
+            state.content = ""
+    except json.JSONDecodeError as e:
+        print(str(e) + content)
+    return state
+
+
+# In[ ]:
+from langgraph.graph import StateGraph, MessagesState, START, END
+from langgraph.prebuilt import tools_condition
+
+def call_model(state: MessagesState):
+    messages = state["messages"]
+    response = fix_tool_calls(model_with_tools.invoke(messages))
+    return {"messages": [response]}
+
+workflow = StateGraph(MessagesState)
+
+workflow.add_node("agent", call_model)
+workflow.add_node("tools", tool_node)
+
+workflow.add_edge(START, "agent")
+workflow.add_conditional_edges("agent", tools_condition, ["tools", END])
+workflow.add_edge("tools", "agent")
+
+app = workflow.compile()
+
+
+# In[ ]:
+for chunk in app.stream(
+    {"messages": [("human", "what is the current time?")]}, stream_mode="values"
+):
+    chunk["messages"][-1].pretty_print()
+
+for chunk in app.stream(
+    {"messages": [("human", "what is the weather in SF?")]}, stream_mode="values"
+):
+    chunk["messages"][-1].pretty_print()
